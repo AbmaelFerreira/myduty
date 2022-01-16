5bb991f057a6799b7a1e5e37ada274cc4c02b93e
AbmaelFerreira <abmael_ninha@hotmail.com>
2022-01-15 23:28:58 -0400

    Project Created

---
+++ backend/src/test/java/com/appsystem/myduty/MydutyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsystem.myduty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydutyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
